60dba7248a81b8026099b05f349e87504470a617
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-26 22:48:49 +0500

    add(prisma-commands): new commands for prisma has been added in package.json scripts

--- src/config/prisma.config.ts
+++ src/config/prisma.config.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
+
+import path from 'path';
+
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: path.resolve(process.cwd(), 'prisma/schema.prisma'),
   migrations: {
-    path: 'prisma/migrations',
+    path: path.resolve(process.cwd(), 'prisma/migrations'),
   },
   datasource: {
     url: env('DATABASE_URL'),
