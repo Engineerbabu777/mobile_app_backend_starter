a49718a313a792ef22431275ca0c0b00f43f30fc
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 00:42:12 +0500

    add(signinService): this new service has been added

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,7 +1,7 @@
 import { createUserRepository, findUserByEmailRepository } from '../respository/auth.repository.js';
-import { AuthResponse, SignupInput } from '../types/auth.types.js';
+import { AuthResponse, SigninInput, SignupInput } from '../types/auth.types.js';
 
-import { hashPassword } from '@/src/shared/utils/hash.js';
+import { comparePassword, hashPassword } from '@/src/shared/utils/hash.js';
 import { generateToken } from '@/src/shared/utils/jwt.js';
 
 export const signupService = async (data: SignupInput): Promise<AuthResponse> => {
@@ -10,6 +10,17 @@ export const signupService = async (data: SignupInput): Promise<AuthResponse> =>
 
   const hashed = await hashPassword(data.password);
   const user = await createUserRepository({ ...data, password: hashed });
+
+  const token = generateToken({ id: user.id, email: user.email });
+  return { user: { id: user.id, email: user.email, name: user.name }, token };
+};
+
+export const signinService = async (data: SigninInput): Promise<AuthResponse> => {
+  const user = await findUserByEmailRepository(data.email);
+  if (!user) throw new Error('Invalid credentials');
+
+  const isValid = await comparePassword(data.password, user.password);
+  if (!isValid) throw new Error('Invalid credentials');
 
   const token = generateToken({ id: user.id, email: user.email });
   return { user: { id: user.id, email: user.email, name: user.name }, token };
