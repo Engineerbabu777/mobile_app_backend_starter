75ecaa42ef6026bd7b2353516d1036779996b433
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-26 22:01:25 +0500

    add(prisma): prisma generated file has been created

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style', 'add'],
+    ],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
   },
 };
