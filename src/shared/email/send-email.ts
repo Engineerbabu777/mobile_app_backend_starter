fbe022f024e14fb6e01a4742d9de824f178547db
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 01:42:08 +0500

    add(sendEmailHelper): this has been added new shared helper

---
+++ src/shared/email/send-email.ts
@@ -0,0 +1,22 @@
+import type { SentMessageInfo } from 'nodemailer';
+
+import { mailTransporter } from '@/src/config/mail.config.js';
+
+interface SendEmailParams {
+  to: string;
+  subject: string;
+  html: string;
+}
+
+export const sendEmailHelper = async ({
+  to,
+  subject,
+  html,
+}: SendEmailParams): Promise<SentMessageInfo> => {
+  return mailTransporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to,
+    subject,
+    html,
+  });
+};
