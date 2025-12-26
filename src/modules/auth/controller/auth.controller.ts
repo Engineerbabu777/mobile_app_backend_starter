825f564224a6b3cf26095b5d78008e151385adec
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 01:06:12 +0500

    add(errorHandler): this new error handler has been added

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import { NextFunction, Request, Response } from 'express';
 
