4fe82104a8980e05bba4932c7258ca2d0cf53322
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 02:27:10 +0500

    add(login): this new route ahs been added

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
-import { signupController } from '@/src/modules/auth/controller/auth.controller.js';
-import { signupSchema } from '@/src/modules/auth/schema/auth.schema.js';
+import {
+  loginController,
+  signupController,
+} from '@/src/modules/auth/controller/auth.controller.js';
+import { signinSchema, signupSchema } from '@/src/modules/auth/schema/auth.schema.js';
 import { validateRequest } from '@/src/shared/middlewares/validate-request.js';
 
 const router = Router();
 
 router.post('/signup', validateRequest(signupSchema), signupController);
+router.post('/login', validateRequest(signinSchema), loginController);
 
 export default router;
