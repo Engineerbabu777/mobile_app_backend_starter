11cd8901cda58c14ebe47b28d393769d4b881404
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 01:15:01 +0500

    add(loginController): this new login controller has been added

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import { NextFunction, Request, Response } from 'express';
 
-import { signupService } from '../service/auth.service.js';
-import { SignupInput } from '../types/auth.types.js';
+import { signinService, signupService } from '../service/auth.service.js';
+import { SigninInput, SignupInput } from '../types/auth.types.js';
 
 export const signupController = async (
   req: Request<{}, {}, SignupInput>,
@@ -18,6 +13,23 @@ export const signupController = async (
     const body = req.body;
     const result = await signupService(body);
     return res.status(201).json({ success: true, data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const loginController = async (
+  req: Request<{}, {}, SigninInput>,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const result = await signinService(req.body);
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
   } catch (err) {
     next(err);
   }
