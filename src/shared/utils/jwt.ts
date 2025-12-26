42b470a1f6333c0f6c09d6a296a7457806a592a4
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-26 23:15:07 +0500

    add(generateToken): this has been added new method

---
+++ src/shared/utils/jwt.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import jwt from 'jsonwebtoken';
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('FATAL_ERROR: JWT_SECRET environment variable is not defined.');
+}
+const JWT_SECRET: string = process.env.JWT_SECRET;
+
+const JWT_EXPIRES_IN = '1h';
+
+export const generateToken = (payload: object): string => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+};
+
+// export const verifyToken = (token: string): JwtPayload | null => {
+//   try {
+//     return jwt.verify(token, JWT_SECRET) as JwtPayload;
+//   } catch (error) {
+//     return null;
+//   }
+// };
