5194a55e7fbc66b55eb67caf2aa293498c2a5306
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-26 23:57:47 +0500

    add(findUserByEmail): this new method has been added

--- src/modules/auth/respository/auth.repository.ts
+++ src/modules/auth/respository/auth.repository.ts
@@ -6,3 +6,7 @@ import { prisma } from '@/src/shared/database/prisma.js';
 export const createUserRepository = async (data: SignupInput): Promise<User> => {
   return prisma.user.create({ data });
 };
+
+export const findUserByEmail = async (email: string): Promise<User | null> => {
+  return prisma.user.findUnique({ where: { email } });
+};
