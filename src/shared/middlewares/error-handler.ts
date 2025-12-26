54ffaa75b1eeef672d46281aacfc1a9cab575ac7
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 01:08:24 +0500

    add(errorHandler): this new error handler has been added

--- src/shared/middlewares/error-handler.ts
+++ src/shared/middlewares/error-handler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
