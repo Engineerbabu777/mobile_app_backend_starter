169c663deccbd5c8a9a2f8c216331de6e57b36a3
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 03:07:16 +0500

    add(app): this has been added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-const startServer = (): void => {
-  console.log('🚀 Server started');
-};
+import express from 'express';
 
-startServer();
+import authRoutes from './modules/auth/routes/auth.routes.js';
+
+import { corsMiddleware } from '@/src/shared/middlewares/cors.js';
+import { errorHandler } from '@/src/shared/middlewares/error-handler.js';
+
+const app = express();
+
+app.use(corsMiddleware);
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.use(errorHandler);
+
+export default app;
