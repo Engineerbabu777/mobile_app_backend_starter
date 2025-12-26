c45b4f143d70c070eef1ab6f476105072d5eee58
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-27 00:12:16 +0500

    add(createPasswordResetToken): this new Respository has been added

--- src/modules/auth/respository/auth.repository.ts
+++ src/modules/auth/respository/auth.repository.ts
@@ -1,16 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { SignupInput } from '../types/auth.types.js';
 
-import { User } from '@/prisma/generated/prisma/client.js';
+import { PasswordResetToken, User } from '@/prisma/generated/prisma/client.js';
 import { prisma } from '@/src/shared/database/prisma.js';
 
 export const createUserRepository = async (data: SignupInput): Promise<User> => {
-  return prisma.user.create({ data });
+  return await prisma.user.create({ data });
 };
 
 export const findUserByEmail = async (email: string): Promise<User | null> => {
-  return prisma.user.findUnique({ where: { email } });
+  return await prisma.user.findUnique({ where: { email } });
 };
 
 export const updateUserPassword = async (userId: string, password: string): Promise<User> => {
-  return prisma.user.update({ where: { id: userId }, data: { password } });
+  return await prisma.user.update({ where: { id: userId }, data: { password } });
+};
+
+export const createPasswordResetToken = async (
+  userId: string,
+  token: string,
+  expiresAt: Date,
+): Promise<PasswordResetToken> => {
+  return await prisma.passwordResetToken.create({ data: { userId, token, expiresAt } });
 };
