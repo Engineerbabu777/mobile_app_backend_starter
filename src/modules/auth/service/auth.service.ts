efcda8f637c9eca7c023a641b89086dc7b9fae20
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-28 16:58:46 +0500

    fix(chnages): a bit of changes has been made

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { addMinutes } from 'date-fns';
 
 import {
