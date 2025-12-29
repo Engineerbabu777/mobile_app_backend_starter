1e99ae0f83a77f3ff54718b1bf970d9c6cb8b763
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-29 10:11:42 +0500

    fix(issues): @ts-ignoresrc/shared/database/prisma.ts fixed

--- src/shared/database/prisma.ts
+++ src/shared/database/prisma.ts
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-// @ts-ignore
 import { PrismaClient } from '@/prisma/generated/prisma/client.js';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
