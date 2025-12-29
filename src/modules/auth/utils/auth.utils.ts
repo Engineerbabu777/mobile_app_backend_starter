59226bce1265cb15fdda573a7200f66a06a22478
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-29 17:21:34 +0500

    add(auth-utils): these are just updated

--- src/modules/auth/utils/auth.utils.ts
+++ src/modules/auth/utils/auth.utils.ts
@@ -1,9 +1,15 @@
-import { sendEmailHelper } from '@/src/shared/email/send-email.js';
+import { sendEmailViaSendGrid } from '@/src/shared/email/send-email.js';
 import { resetPasswordHtmlTemplate } from '@/src/shared/email/templates/reset-password-email-template.js';
 import { verificationEmailTemplate } from '@/src/shared/email/templates/verification-email-template.js';
 
 export const sendVerificationEmailUtil = async (email: string, code: string): Promise<void> => {
-  await sendEmailHelper({
+  // await sendEmailHelper({
+  //   to: email,
+  //   subject: 'Verify your email',
+  //   html: verificationEmailTemplate(code),
+  // });
+
+  await sendEmailViaSendGrid({
     to: email,
     subject: 'Verify your email',
     html: verificationEmailTemplate(code),
@@ -11,9 +17,15 @@ export const sendVerificationEmailUtil = async (email: string, code: string): Pr
 };
 
 export const sendResetPasswordEmailUtil = async (email: string, code: string): Promise<void> => {
-  await sendEmailHelper({
+  // await sendEmailHelper({
+  //   to: email,
+  //   subject: 'Verify your email',
+  //   html: resetPasswordHtmlTemplate(code),
+  // });
+
+  await sendEmailViaSendGrid({
     to: email,
-    subject: 'Verify your email',
+    subject: 'Reset password Email',
     html: resetPasswordHtmlTemplate(code),
   });
 };
